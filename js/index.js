f644dd07c29953f5bacfad81943ae875c2391f49
Will Marttala <wmarttala@admins-MacBook-Pro-5.local>
2018-06-25 16:06:30 -0500

    Day 1 notes

---
+++ js/index.js
@@ -0,0 +1,10 @@
+const contactModal = document.getElementById('contactModal')
+// const contactModalLink = document.getElementById('contactModalLink')
+
+const showModal = () => {
+  contactModal.classList.add('is-active')
+}
+
+const hideModal = () => {
+  contactModal.classList.remove('is-active')
+}
